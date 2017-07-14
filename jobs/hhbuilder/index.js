018a8987b3ac4dd6e10fd2a5caa1d4cf96a59b07
Nicole Hemenway <nkhemenway@gmail.com>
2017-07-13 17:39:22 -0700

    add funciton comments

--- jobs/hhbuilder/index.js
+++ jobs/hhbuilder/index.js
@@ -8,68 +8,25 @@
   // * Display the household list in the HTML as it is modified
   // * Serialize the household as JSON upon form submission as a fake trip to the server
 
+var householdListEl = document.getElementsByClassName("household")[0];
+var formEl = document.querySelector("form");
+var ageEl = document.getElementsByName("age")[0];
+var relEl = document.getElementsByName("rel")[0];
+var smokerEl = document.getElementsByName("smoker")[0];
+var addBtnEl = document.getElementsByClassName('add')[0];
+var submitBtnEl = document.querySelector('button[type="submit"]');
+
+addBtnEl.addEventListener("click", onAddBtnClick);
+
+//create a ul for error messages
+var errorsEl = document.createElement("UL");
+errorsEl.setAttribute("id", "error-messages");
+formEl.insertBefore(errorsEl, formEl.childNodes[0]);
+
 var household = [];
 var errorMessages = [];
 
-function validateInput(){
-  errorMessages = [];
-
-  if( ageEl.value === "" ){
-    errorMessages.push("Age is required field");
-  } else if (!parseInt(ageEl.value) || parseInt(ageEl.value) < 0){
-    errorMessages.push("Invalid age input");
-  }
-
-  if( relEl.value === "" ){
-    errorMessages.push("Relationship is required field");
-  }
-
-}
-
-function displayHousehold(){
-  //clear all li elements from household ol
-  while (householdListEl.firstChild) {
-    householdListEl.removeChild(householdListEl.firstChild);
-  }
-
-  //update household display
-  for(var i = 0; i < household.length ; i++){
-    var newHouseholdLi = document.createElement("LI");
-    var newLiTextNode = document.createTextNode("Age: " + household[i].age + ", Rel: " + household[i].rel + ", Smoker: " + household[i].smoker );
-    newHouseholdLi.appendChild(newLiTextNode);
-
-    var deleteBtnEl = document.createElement("BUTTON");
-    deleteBtnEl.innerHTML = "Delete";
-    deleteBtnEl.setAttribute("class", "delete");
-    deleteBtnEl.setAttribute("id", i);
-    deleteBtnEl.addEventListener("click", onDeleteBtnClick);
-    newHouseholdLi.appendChild(deleteBtnEl);
-
-    householdListEl.appendChild(newHouseholdLi);
-  }
-}
-
-function displayErrorMessages(){
-  while (errorsEl.firstChild) {
-    errorsEl.removeChild(errorsEl.firstChild);
-  }
-
-  for(var i = 0; i < errorMessages.length ; i++){
-    var errorLiNode = document.createElement("LI");
-    var textnode = document.createTextNode(errorMessages[i]);
-    errorLiNode.appendChild(textnode);
-    errorsEl.appendChild(errorLiNode);
-  }
-}
-
-function clearErrorMessages(){
-  errorMessages = [];
-
-  while (errorsEl.firstChild) {
-    errorsEl.removeChild(errorsEl.firstChild);
-  }
-}
-
+//validates input, and if valid, add new member details to hosuehold array
 function onAddBtnClick (e){
   e.preventDefault();
 
@@ -97,26 +54,62 @@ function onDeleteBtnClick(e){
   displayHousehold();
 }
 
-var householdListEl = document.getElementsByClassName("household")[0];
+//populates errorMessages array with informative error messages
+function validateInput(){
+  errorMessages = [];
 
-var formEl = document.querySelector("form");
-var ageEl = document.getElementsByName("age")[0];
-var relEl = document.getElementsByName("rel")[0];
-var smokerEl = document.getElementsByName("smoker")[0];
+  if( ageEl.value === "" ){
+    errorMessages.push("Age is required field");
+  } else if (!parseInt(ageEl.value) || parseInt(ageEl.value) < 0){
+    errorMessages.push("Invalid age input");
+  }
 
-var ageValue = ageEl.value;
-var relValue = relEl.value;
-var smokerValue = smokerEl.value;
+  if( relEl.value === "" ){
+    errorMessages.push("Relationship is required field");
+  }
+}
 
-var addBtnEl = document.getElementsByClassName('add')[0];
-var submitBtnEl = document.querySelector('button[type="submit"]');
-// var ageValue = document.getElementById("uniqueID").value;
-// var relationshipValue = document.getElementById("uniqueID").value;
-// var isSmokerValue = document.getElementById("uniqueID").value;
-addBtnEl.addEventListener("click", onAddBtnClick);
-addBtnEl.addEventListener("click", onAddBtnClick);
+//clears all li elements from household ol, and repopulates the list with current household data
+function displayHousehold(){
+  while (householdListEl.firstChild) {
+    householdListEl.removeChild(householdListEl.firstChild);
+  }
 
-//create a ul for error messages
-var errorsEl = document.createElement("UL");
-errorsEl.setAttribute("id", "error-messages");
-formEl.insertBefore(errorsEl, formEl.childNodes[0]);
+  for(var i = 0; i < household.length ; i++){
+    var newHouseholdLi = document.createElement("LI");
+    var newLiTextNode = document.createTextNode("Age: " + household[i].age + ", Rel: " + household[i].rel + ", Smoker: " + household[i].smoker );
+    newHouseholdLi.appendChild(newLiTextNode);
+
+    var deleteBtnEl = document.createElement("BUTTON");
+    deleteBtnEl.innerHTML = "Delete";
+    deleteBtnEl.setAttribute("class", "delete");
+    deleteBtnEl.setAttribute("id", i);
+    deleteBtnEl.addEventListener("click", onDeleteBtnClick);
+    newHouseholdLi.appendChild(deleteBtnEl);
+
+    householdListEl.appendChild(newHouseholdLi);
+  }
+}
+
+//displays all error messages that validateInput collected in the errorMessages array
+function displayErrorMessages(){
+  while (errorsEl.firstChild) {
+    errorsEl.removeChild(errorsEl.firstChild);
+  }
+
+  for(var i = 0; i < errorMessages.length ; i++){
+    var errorLiNode = document.createElement("LI");
+    var textnode = document.createTextNode(errorMessages[i]);
+    errorLiNode.appendChild(textnode);
+    errorsEl.appendChild(errorLiNode);
+  }
+}
+
+//clears errorMessages array, as well as the errors list shown in the browser
+function clearErrorMessages(){
+  errorMessages = [];
+
+  while (errorsEl.firstChild) {
+    errorsEl.removeChild(errorsEl.firstChild);
+  }
+}
