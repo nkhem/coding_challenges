47b170cffef4625a5de66447382070de8f1ae5e7
Nicole Hemenway <nkhemenway@gmail.com>
2017-07-13 23:23:22 -0700

    get closer

--- jobs/slcsp/calculateSlcsp.rb
+++ jobs/slcsp/calculateSlcsp.rb
@@ -8,7 +8,11 @@ plans_data = Hash.new() #keys: state/rate_area, value: metal_level, rate
 CSV.foreach("./plans.csv", :headers => true, :header_converters => :symbol, :converters => :all) do |row|
   plans_data[row.fields[1]] ||= {}
   plans_data[row.fields[1]][row.fields[-1]] ||= []
-  plans_data[row.fields[1]][row.fields[-1]].push(Hash[row.headers[2..-2].zip(row.fields[2..-2])])
+  if(row.fields[2] == "Silver")
+    # push the current rate into arr, then from among them select the two lowest unique values
+    plans_data[row.fields[1]][row.fields[-1]] =
+      plans_data[row.fields[1]][row.fields[-1]].push(row.fields[-2]).uniq.min(2)
+  end
 end
 
 zips_data = {} #keys: zipcode, values: :state, :county_code, :name, :rate_area
@@ -18,15 +22,24 @@ end
 
 slcsp_data[1..-1].each do |row|
   zipcode = row[0]
-  # debugger
-  state = zips_data[zipcode][:state]
-  rate_area = zips_data[zipcode][:rate_area]
-  p plans_data[state][rate_area].size
+
+  if(zips_data[zipcode] && zips_data[zipcode][:state] && zips_data[zipcode][:rate_area])
+    state = zips_data[zipcode][:state]
+    rate_area = zips_data[zipcode][:rate_area]
+  else
+    break
+  end
+
+  if(plans_data[state] && plans_data[state][rate_area])
+    row[1] = plans_data[state][rate_area][1]
+  else
+    break
+  end
 end
-#
-# CSV.open("./slcsp.csv", "wb") do |csv|
-#   slcsp_data.each { |row| csv << row }
-# end
+
+CSV.open("./slcsp.csv", "wb") do |csv|
+  slcsp_data.each { |row| csv << row }
+end
 
 # p slcsp_data
 # p "---"
