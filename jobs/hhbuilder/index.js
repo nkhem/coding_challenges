804e35627231b2312f0871b593f5a1b29ec10a66
Nicole Hemenway <nkhemenway@gmail.com>
2017-07-13 17:06:41 -0700

    add validations

--- jobs/hhbuilder/index.js
+++ jobs/hhbuilder/index.js
@@ -6,35 +6,25 @@
 // * Display the household list in the HTML as it is modified
 // * Serialize the household as JSON upon form submission as a fake trip to the server
 var household = [];
+var errorMessages = [];
 
-function displayErrorMessages(){
-  var newDiv = document.createElement("div");
-  var newContent = document.createTextNode("Required field");
-  newDiv.appendChild(newContent); //add the text node to the newly created div.
+function validateInput(){
+  errorMessages = [];
 
-  ageEl.insertBefore(newDiv);
+  if( ageEl.value === "" ){
+    errorMessages.push("Age is required field");
+  } else if (!parseInt(ageEl.value) || parseInt(ageEl.value) < 0){
+    errorMessages.push("Invalid age input");
+  }
+
+  if( relEl.value === "" ){
+    errorMessages.push("Relationship is required field");
+  }
+
 }
 
-function addNewMemberToHousehold(){
-
-}
-
-function displayCurrentHousehold(){
-
-}
-
-function onAddBtnClick (e){
-  e.preventDefault();
-
-  //add the new member to the household
-  var newMember = {
-    age: ageEl.value,
-    rel: relEl.value,
-    smoker: smokerEl.checked ? "true" : "false"
-  };
-  household.push(newMember);
-
-  //clear household ol
+function rerenderHousehold(){
+  //clear all li elements from household ol
   while (householdListEl.firstChild) {
     householdListEl.removeChild(householdListEl.firstChild);
   }
@@ -47,10 +37,46 @@ function onAddBtnClick (e){
 
     householdListEl.appendChild(node);
   }
+}
 
-  // displayErrorMessages();
+function displayErrorMessages(){
+  while (errorsEl.firstChild) {
+    errorsEl.removeChild(errorsEl.firstChild);
+  }
 
-  formEl.reset();
+  for(var i = 0; i < errorMessages.length ; i++){
+    var errorLiNode = document.createElement("LI");
+    var textnode = document.createTextNode(errorMessages[i]);
+    errorLiNode.appendChild(textnode);
+    errorsEl.appendChild(errorLiNode);
+  }
+}
+
+function clearErrorMessages(){
+  errorMessages = [];
+  
+  while (errorsEl.firstChild) {
+    errorsEl.removeChild(errorsEl.firstChild);
+  }
+}
+
+function onAddBtnClick (e){
+  e.preventDefault();
+
+  validateInput();
+
+  if( errorMessages.length === 0 ){
+    clearErrorMessages();
+    household.push({
+      age: ageEl.value,
+      rel: relEl.value,
+      smoker: smokerEl.checked ? "true" : "false"
+    });
+    rerenderHousehold();
+    formEl.reset();
+  } else {
+    displayErrorMessages();
+  }
 
 }
 
@@ -71,3 +97,8 @@ var submitBtnEl = document.querySelector('button[type="submit"]');
 // var relationshipValue = document.getElementById("uniqueID").value;
 // var isSmokerValue = document.getElementById("uniqueID").value;
 addBtnEl.addEventListener("click", onAddBtnClick);
+
+//create a ul for error messages
+var errorsEl = document.createElement("UL");
+errorsEl.setAttribute("id", "error-messages");
+formEl.insertBefore(errorsEl, formEl.childNodes[0]);
